2c61c7d59622ef6f43fcec355cbd69bc15f35816
roguepathclothing-dot <roguepathclothing@gmail.com>
2025-09-03 14:50:45 +0530

    Add announcement bar JS

---
+++ assets/announcement-bar.js
@@ -0,0 +1,5 @@
+// Announcement Bar logic
+document.addEventListener("DOMContentLoaded", function() {
+  const bar = document.querySelector('.announcement-bar');
+  if(bar) bar.style.display = 'block';
+});
